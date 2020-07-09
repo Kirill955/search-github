0fe751e6d52f0a1d05f9892fb36c104376e6f612
Кирилл Омаров <kirill.kirill.omarov@mail.ru>
2020-07-09 09:27:17 +0600

    Добавил обертку для карточек

---
+++ src/components/CardsWrapper/CardsWrapper.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Style
+import './CardsWrapper.scss';
+
+// Components
+import CardItem from '../CardItem';
+
+export const CardsWrapper = ({ repositories }) => {
+  return (
+    <div>
+      { repositories.map(repos => <CardItem repos={repos} />) } 
+    </div>
+  )
+};
